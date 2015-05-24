49eaae1e53b15b8b142b5535118158950e46bd84
Kirit Sælensminde <k@kirit.com>
2015-05-24 14:23:44 +0700

    Start to sketch out how tenants are going to be dealt with.

---
+++ include/rask/tenants.hpp
@@ -0,0 +1,20 @@
+/*
+    Copyright 2015, Proteus Tech Co Ltd. http://www.kirit.com/Rask
+    Distributed under the Boost Software License, Version 1.0.
+    See accompanying file LICENSE_1_0.txt or copy at
+        http://www.boost.org/LICENSE_1_0.txt
+*/
+
+
+#pragma once
+
+
+namespace rask {
+
+
+    /// Check the configuration for changes in tenants
+    void tenants(const fostlib::json &dbconfig);
+
+
+}
+
