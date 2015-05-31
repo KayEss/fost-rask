d876e7d490ce4906c2f0809b8a6ab83de6643089
Kirit Sælensminde <k@kirit.com>
2015-05-31 17:36:25 +0700

    Use big endian on the wire.

--- src/connection.cpp
+++ src/connection.cpp
@@ -14,6 +14,7 @@
 #include <fost/log>
 
 #include <boost/asio/spawn.hpp>
+#include <boost/endian/conversion.hpp>
 
 #include <mutex>
 
@@ -44,8 +45,10 @@ namespace {
             rask::tick time(rask::tick::now());
             std::array<unsigned char, 3 + 8 + 4> data{
                 data.size() - 2, 0x80, rask::known_version};
-            std::memcpy(data.data() + 3, &time.time, 8);
-            std::memcpy(data.data() + 3 + 8, &time.server, 4);
+            int64_t tick = boost::endian::native_to_big(time.time);
+            int32_t server = boost::endian::native_to_big(time.server);
+            std::memcpy(data.data() + 3, &tick, 8);
+            std::memcpy(data.data() + 3 + 8, &server, 4);
             async_write(socket->cnx, boost::asio::buffer(data), sender);
         } else {
             static unsigned char data[] = {0x01, 0x80, rask::known_version};
@@ -96,7 +99,9 @@ void rask::read_and_process(std::shared_ptr<rask::connection> socket) {
                     int64_t time = 0; int32_t server = 0;
                     if ( --packet_size ) {
                         socket->input_buffer.sgetn(reinterpret_cast<char*>(&time), 8);
+                        time = boost::endian::big_to_native(time);
                         socket->input_buffer.sgetn(reinterpret_cast<char*>(&server), 4);
+                        server = boost::endian::big_to_native(server);
                         tick::overheard(time, server);
                         packet_size -= 12;
                         while ( packet_size-- ) socket->input_buffer.sbumpc();
