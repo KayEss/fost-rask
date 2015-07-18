180e08b046237126a4d205242b9e61cccfc80010
Kirit Sælensminde <k@kirit.com>
2015-07-18 09:40:18 +0700

    Start to impose a proper structure on sending tenant data.

--- src/connection.tenant.cpp
+++ src/connection.tenant.cpp
@@ -32,33 +32,43 @@ rask::connection::out rask::tenant_packet(
 
 namespace {
     void send_tenant_content(
-        std::shared_ptr<rask::tenant> tenant, std::shared_ptr<rask::connection> socket
+        std::shared_ptr<rask::tenant> tenant,
+        std::shared_ptr<rask::connection> socket,
+        std::size_t layer, const rask::name_hash_type &prefix
     ) {
-        for ( auto iter(tenant->subscription->inodes().begin()); iter != tenant->subscription->inodes().end(); ++iter ) {
-            const fostlib::json inode(*iter);
-            auto &filetype = inode["filetype"];
-            if ( filetype == rask::tenant::directory_inode ) {
-                fostlib::log::debug(rask::c_fost_rask)
-                    ("", "sending create_directory")
-                    ("inode", inode);
-                socket->queue(
-                    [tenant, inode]() {
-                        return create_directory_out(*tenant, rask::tick(inode["priority"]),
-                            fostlib::coerce<fostlib::string>(inode["name"]));
-                    });
-            } else if ( filetype == rask::tenant::move_inode_out ) {
-                fostlib::log::debug(rask::c_fost_rask)
-                    ("", "sending move_out")
-                    ("inode", inode);
-                socket->queue(
-                    [tenant, inode]() {
-                        return move_out_packet(*tenant, rask::tick(inode["priority"]),
-                            fostlib::coerce<fostlib::string>(inode["name"]));
-                    });
-            } else {
-                fostlib::log::error(rask::c_fost_rask)
-                    ("", "Unkown inode type to send to peer")
-                    ("inode", inode);
+        auto dbp = tenant->subscription->inodes().layer_dbp(layer, prefix);
+        fostlib::jsondb::local db(*dbp);
+        if ( rask::partitioned(db) ) {
+            throw fostlib::exceptions::not_implemented(
+                "Sending a partitioned part of the tenant tree");
+        } else {
+            auto inodes = db["inodes"];
+            for ( auto iter(inodes.begin()); iter != inodes.end(); ++iter ) {
+                const fostlib::json inode(*iter);
+                auto &filetype = inode["filetype"];
+                if ( filetype == rask::tenant::directory_inode ) {
+                    fostlib::log::debug(rask::c_fost_rask)
+                        ("", "sending create_directory")
+                        ("inode", inode);
+                    socket->queue(
+                        [tenant, inode]() {
+                            return create_directory_out(*tenant, rask::tick(inode["priority"]),
+                                fostlib::coerce<fostlib::string>(inode["name"]));
+                        });
+                } else if ( filetype == rask::tenant::move_inode_out ) {
+                    fostlib::log::debug(rask::c_fost_rask)
+                        ("", "sending move_out")
+                        ("inode", inode);
+                    socket->queue(
+                        [tenant, inode]() {
+                            return move_out_packet(*tenant, rask::tick(inode["priority"]),
+                                fostlib::coerce<fostlib::string>(inode["name"]));
+                        });
+                } else {
+                    fostlib::log::error(rask::c_fost_rask)
+                        ("", "Unkown inode type to send to peer")
+                        ("inode", inode);
+                }
             }
         }
     }
@@ -80,7 +90,7 @@ void rask::tenant_packet(connection::in &packet) {
             [socket = packet.socket, name = std::move(name), hash = std::move(hash)]() {
                 auto tenant = known_tenant(socket->workers, name);
                 if ( tenant->subscription ) {
-                    send_tenant_content(tenant, socket);
+                    send_tenant_content(tenant, socket, 0u, name_hash_type());
                 } else {
                     // We're not subscribed to this, so we just store the hash in our
                     // tenants database so we can use it to calculate our server hash
